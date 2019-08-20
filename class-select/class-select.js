2e73b03d827bb4eb82d7a6a766e20438293df3a6
skilla <38085777+myowncorp@users.noreply.github.com>
2019-08-19 20:32:22 -0400

    Check the Error

--- class-select/class-select.js
+++ class-select/class-select.js
@@ -5,19 +5,18 @@
 
 // INITIALIZES THE EVENT LISTENERS 
 //
-debugger;
-var warrClassSelectObj = document.getElementById("warr-class-select").addEventListener("click", function (){
+var warrClassSelectObj = document.getElementById("warr-select-box").addEventListener("click", function (){
 	window.location.href = "../play-page/play-page.html";
-	setPageWarr();
+	// setPageWarr();
 });
 
-var mageClassSelectObj = document.getElementById("mage-class-select").addEventListener("click", function(){
-	window.location.href = "..play-page/play-page.html";
-	setPageMage();
-})
+var mageClassSelectObj = document.getElementById("mage-select-box").addEventListener("click", function(){
+	window.location.href = "../play-page/play-page.html";
+	// setPageMage();
+});
 
 
-var archerClassSelectObj = document.getElementById("archer-class-select").addEventListener("click", function(){
-	window.location.href = "..play-page/play-page.htm";
-	setPageArcher();
-})
+var archerClassSelectObj = document.getElementById("archer-select-box").addEventListener("click", function(){
+	window.location.href = "../play-page/play-page.html";
+	// setPageArcher();
+});
